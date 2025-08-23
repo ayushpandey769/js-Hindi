4f78654ace711629a84be0c0becd5dfe3c25710d
ayushpandey769 <118703718+ayushpandey769@users.noreply.github.com>
2025-08-23 12:19:17 +0000

    comparisons

--- 01_Basics/03_conversionOperations.js
+++ 01_Basics/03_conversionOperations.js
@@ -1,72 +1,118 @@
 // let score = "33"
 
-// console.log(typeof score);
+// // console.log(typeof score);
 
 // let valueInNumber = Number(score)
-// console.log(typeof valueInNumber);
+// // console.log(typeof valueInNumber);
 
 // let score = "33abc"
 
-// console.log(typeof score);
+// // console.log(typeof score);
 
 // let valueInNumber = Number(score)
-// console.log(typeof valueInNumber);
-// console.log(valueInNumber);
+// // console.log(typeof valueInNumber);
+// // console.log(valueInNumber);
 
 // let score = null
 
-// console.log(typeof score);
+// // console.log(typeof score);
 
 // let valueInNumber = Number(score)
-// console.log(typeof valueInNumber);
-// console.log(valueInNumber);
+// // console.log(typeof valueInNumber);
+// // console.log(valueInNumber);
 
 // let score = undefined
 
-// console.log(typeof score);
+// // console.log(typeof score);
 
 // let valueInNumber = Number(score)
-// console.log(typeof valueInNumber);
-// console.log(valueInNumber);
+// // console.log(typeof valueInNumber);
+// // console.log(valueInNumber);
 
 
 // let score = "harsh"
 
-// console.log(typeof score);
+// // console.log(typeof score);
 
 // let valueInNumber = Number(score)
-// console.log(typeof valueInNumber);
-// console.log(valueInNumber);
+// // console.log(typeof valueInNumber);
+// // console.log(valueInNumber);
 
 
 
-/*
-"33" => 33
-"33abc" => NaN
-true => 1 , false => 0
+// 
+// "33" => 33
+// "33abc" => NaN
+// true => 1 , false => 0
 
 
-*/
+// 
 
     
     
 // let isLoggedIn = ""  //empty string
 
 // let booleanIsLoggedIn = Boolean(isLoggedIn)
-//     console.log(typeof booleanIsLoggedIn);
-//     console.log(booleanIsLoggedIn);
+// //     console.log(typeof booleanIsLoggedIn);
+// //     console.log(booleanIsLoggedIn);
 
-    // 1 => true
-    // 0 => false
-    // "" => false
-    // "harsh" => true
+//     // 1 => true
+//     // 0 => false
+//     // "" => false
+//     // "harsh" => true
 
 
-    let someNumber = 33
+//     let someNumber = 33
 
-    let stringNumber = String(someNumber)
-    console.log(typeof stringNumber);
-    console.log(stringNumber);
+//     let stringNumber = String(someNumber)
+//     // console.log(typeof stringNumber);
+//     // console.log(stringNumber);
     
+
+
+    //***************************Operations****************** */
+
+
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**3);
+// console.log(2%3);
+
+
+let str1 = "Hello"
+let str2 = " Harsh"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("2" + 1);
+// console.log(2 + "1");
+// console.log("2" + "1");
+// console.log("2" + 1 + 2);
+// console.log(2 + 1 + "2");
+
+
+// console.log(+true);
+// console.log(+"");
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
+
+
+
+
+
+
+
+
+
+
     
     
