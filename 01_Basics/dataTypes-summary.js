4168d4b4ba428fde0211071e11d4e97d9876ce22
ayushpandey769 <118703718+ayushpandey769@users.noreply.github.com>
2025-08-23 13:16:33 +0000

    Stack&Heap

--- 01_Basics/dataTypes-summary.js
+++ 01_Basics/dataTypes-summary.js
@@ -1,37 +1,62 @@
-//Primitive
+// //Primitive
 
 
-// 7types : Strings , Number , Boolean , null , undefined , Symbol , BigInt
+// // 7types : Strings , Number , Boolean , null , undefined , Symbol , BigInt
 
-// const id = Symbol('123')
-// const anotherId = Symbol('123')
+// // const id = Symbol('123')
+// // const anotherId = Symbol('123')
 
-// console.log(id);
-// console.log(anotherId);
+// // console.log(id);
+// // console.log(anotherId);
 
-// console.log(id === anotherId);
-
-
-const bigNumber = 45698775212332147n
+// // console.log(id === anotherId);
 
 
+// const bigNumber = 45698775212332147n
 
 
-//Reference (Non primitive)
-
-// Arrays , Objects , Functions
-
-const Heros = ["Shaktimaan" , "Naagraj" , "Doga"];
 
 
-let myObj = {
-    name : "Harsh",
-    age : 22
-}
+// //Reference (Non primitive)
 
-const myFunction = function(){
-    console.log("Hello World");
+// // Arrays , Objects , Functions
+
+// const Heros = ["Shaktimaan" , "Naagraj" , "Doga"];
+
+
+// let myObj = {
+//     name : "Harsh",
+//     age : 22
+// }
+
+// const myFunction = function(){
+//     console.log("Hello World");
     
+// }
+
+// console.log(typeof myObj);
+
+//++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack (Primitive) , Heap (Non-primitive)
+
+
+let myName = "Harsh Pandey"
+
+let anotherName = myName
+anotherName = "Pandeyji"
+
+console.log(myName);
+console.log(anotherName);
+
+let userOne = {
+    email : "user@google.com",
+    upi : "user@ybl"
 }
 
-console.log(typeof myObj);
+let userTwo = userOne
+
+userTwo.email = "ayush@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
