aa12db92ec770004861023e77bfc574e7569f12f
ayushpandey769 <118703718+ayushpandey769@users.noreply.github.com>
2025-08-23 12:56:39 +0000

    dataTypes-summary

---
+++ 01_Basics/dataTypes-summary.js
@@ -0,0 +1,37 @@
+//Primitive
+
+
+// 7types : Strings , Number , Boolean , null , undefined , Symbol , BigInt
+
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
+
+// console.log(id);
+// console.log(anotherId);
+
+// console.log(id === anotherId);
+
+
+const bigNumber = 45698775212332147n
+
+
+
+
+//Reference (Non primitive)
+
+// Arrays , Objects , Functions
+
+const Heros = ["Shaktimaan" , "Naagraj" , "Doga"];
+
+
+let myObj = {
+    name : "Harsh",
+    age : 22
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+    
+}
+
+console.log(typeof myObj);
