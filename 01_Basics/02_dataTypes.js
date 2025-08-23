bebbb38970adce3fa8101a03222a2f3467e4d34f
ayushpandey769 <118703718+ayushpandey769@users.noreply.github.com>
2025-08-23 08:47:12 +0000

    datatypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict";  //treat all JS code as newer version
+
+//alert( 3 + 3); we are using nodeJS not browser
+
+console.log( 3 + 
+    3) //code readability
+
+    console.log("ayush")
+
+    let name = "Harsh"
+    let age = 22
+    let isLoggedIn = false
+      //number <= 2 to power 53
+      //bigint
+      //string => ""
+      //boolean => true/false
+      //null => standalone value
+      //undefines =>
+      //symbol => unique
+
+      //object
+
+      console.log(typeof "Harsh");
+
+            console.log(typeof age);
+             console.log(typeof null);
+              console.log(typeof undefined);
+
+
+
