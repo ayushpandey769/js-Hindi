d4ce0c94c3981003a65edf54f63c6e3d72b0a0a4
ayushpandey769 <118703718+ayushpandey769@users.noreply.github.com>
2025-08-23 12:18:24 +0000

    Comparisons

---
+++ 01_Basics/04_Comparison.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+//Comparison and equality check operators works differently in js
+//comparison changes null value to 0...so it gives 'true' in 3rd situation
+
+
+//strict check => checks dataTypes also
+console.log("2" === 2);
+
+
