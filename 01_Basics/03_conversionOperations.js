37f5311082ba97029bdee133693e0bb10398bfc6
ayushpandey769 <118703718+ayushpandey769@users.noreply.github.com>
2025-08-23 09:20:09 +0000

    Conversions of Datatypes

---
+++ 01_Basics/03_conversionOperations.js
@@ -0,0 +1,72 @@
+// let score = "33"
+
+// console.log(typeof score);
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+
+// let score = "33abc"
+
+// console.log(typeof score);
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// let score = null
+
+// console.log(typeof score);
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// let score = undefined
+
+// console.log(typeof score);
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// let score = "harsh"
+
+// console.log(typeof score);
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+
+/*
+"33" => 33
+"33abc" => NaN
+true => 1 , false => 0
+
+
+*/
+
+    
+    
+// let isLoggedIn = ""  //empty string
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+//     console.log(typeof booleanIsLoggedIn);
+//     console.log(booleanIsLoggedIn);
+
+    // 1 => true
+    // 0 => false
+    // "" => false
+    // "harsh" => true
+
+
+    let someNumber = 33
+
+    let stringNumber = String(someNumber)
+    console.log(typeof stringNumber);
+    console.log(stringNumber);
+    
+    
+    
