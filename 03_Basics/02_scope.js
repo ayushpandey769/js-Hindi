5b0e5b4bba33b137e934e033e8d14cd259d334e2
ayushpandey769 <118703718+ayushpandey769@users.noreply.github.com>
2025-08-27 16:07:23 +0000

    scope

---
+++ 03_Basics/02_scope.js
@@ -0,0 +1,51 @@
+// var c = 300
+let a = 300
+
+if(true){
+    let a = 10
+    const b = 20
+    // console.log("Inner :", a);
+    
+}
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+ function one(){
+    const username = "Harsh"
+
+    function two(){
+        const website = "youtube"
+        // console.log(username);
+    }
+    // console.log(website);
+    
+    two()
+ }
+ one()
+
+ if(true){
+    const username = "Harsh"
+    if(username === "Harsh"){
+        const website = " youtube"
+        // console.log(username + website);   
+    }
+    // console.log(website);
+ }
+//  console.log(username);
+ 
+//************************Interesting********************* */
+
+console.log(addOne(5));
+
+function addOne(num){
+    return num + 1
+}
+
+//Expression
+addTwo(5)
+
+const addTwo = function(num){
+    return num + 2
+}
