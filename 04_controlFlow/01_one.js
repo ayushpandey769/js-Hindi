f091ecd79e74555f703cbdcde96352796bd6e319
ayushpandey769 <118703718+ayushpandey769@users.noreply.github.com>
2025-08-28 09:36:29 +0000

    if else

--- 04_controlFlow/01_one.js
+++ 04_controlFlow/01_one.js
@@ -1,10 +1,55 @@
 //if
 
-if ( 2 == "2"){
-    console.log("Executed");
+const isLoggedIn = true
+const temperature = 41
+
+// if ( temperature === 40 ){
+//     console.log("Less than 50");
     
+// }
+// else {
+//     console.log("Temperature is greater than 50"); 
+// }
+//  console.log("Executed");
+ 
+
+// <, >, <=, >=, ==, !=, ===, !==
+
+// const score = 200
+
+// if ( score > 100 ){
+//     let power = "Fly"
+//     console.log(`User Power : ${power}`);
+    
+// }
+
+//     console.log(`User Power : ${power}`);
+
+// const balance = 1000
+
+// if (balance > 500) console.log("test"),console.log("test2");
+
+// if ( balance < 500 ){
+//     console.log("Less than 500");
+// } else if ( balance < 750 ){
+//     console.log("Less than 750");
+// }else if ( balance < 900 ){
+//     console.log("Less than 900");
+// }else {
+//     console.log("less than 1200");    
+// }
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggrdInFromEmail = true
+
+
+if ( userLoggedIn && debitCard && 2==2 ){
+    console.log("Allow to buy courses");
 }
 
-if (false){
-
+if ( loggedInFromGoogle || loggrdInFromEmail ){
+    console.log("User logged in");
+    
 }
