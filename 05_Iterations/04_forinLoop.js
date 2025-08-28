a2956d6d80e9e4b7c6213eac6d960864f295cc45
ayushpandey769 <118703718+ayushpandey769@users.noreply.github.com>
2025-08-28 18:35:49 +0000

    forin loop

--- 05_Iterations/04_forinLoop.js
+++ 05_Iterations/04_forinLoop.js
@@ -6,6 +6,25 @@ const myObject = {
 }
 
 for (const key in myObject) {
-    console.log(key);
-    
+    // console.log(`${key} shortcut is for ${myObject[key]}`);
 }
+
+const programming = ["js" , "rb" , "py" , "java" , "cpp"]
+
+for (const key in programming) {
+    // console.log(programming[key]); 
+}
+//In forof loop if we use key we directly get the elements of the array
+//But in forin loop we get the keys of tha array if we use key
+
+// const map = new Map()
+// map.set('IN' , "India")
+// map.set('USA' , "United States Of America")
+// map.set('Fr' , "France")
+// map.set('IN' , "India")
+
+// for (const key in map) {
+//     console.log(key);
+    
+// }
+//Cannot iterate map in forin loop
