f84adaf77b27e8ac824f3b34509e7f31b3987fca
ayushpandey769 <118703718+ayushpandey769@users.noreply.github.com>
2025-08-28 14:44:47 +0000

    while & do while loop

---
+++ 05_Iterations/02_two.js
@@ -0,0 +1,22 @@
+let index = 0
+while (index <= 10){
+    // console.log(`Value of index is : ${index}`);
+    index = index + 2
+}
+
+let myArray = ["flash" , "superman" , "batman"]
+
+let i = 0
+while (i < myArray.length) {
+    // const element = myArray[i]
+    // console.log(element);
+    // console.log(`Value is : ${myArray[i]}`);
+    i++
+}
+
+
+let score = 11
+do {
+    console.log(`Score is : ${score}`);
+    score++
+} while (score <= 10);
