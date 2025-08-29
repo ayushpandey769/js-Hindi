d26e2ff84221b19c5981534bc1e2be1d7af9792a
ayushpandey769 <118703718+ayushpandey769@users.noreply.github.com>
2025-08-29 07:34:39 +0000

    filter and map

--- 05_Iterations/06_usingFilter.js
+++ 05_Iterations/06_usingFilter.js
@@ -12,7 +12,7 @@ const myNums = [1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10]
 // const newNums = myNums.filter( (num) => num > 4 )
 
 const newNums = []
-
+//foreach loop
 myNums.forEach( (num) => {
     if (num > 4){
         newNums.push(num)
